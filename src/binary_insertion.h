f2f8acced5a09d9bcdeea8d1f4e93d53fbafc2a8
Sergey Gorlov <gxrlxv@Sergeys-Laptop.local>
2024-09-27 18:40:34 +0300

    impl

---
+++ src/binary_insertion.h
@@ -0,0 +1,8 @@
+#ifndef BINARY_INSERTION_H
+#define BINARY_INSERTION_H
+
+#include <vector>
+
+std::vector<int> binary_insertion(const std::vector<int>& array, int value);
+
+#endif // BINARY_INSERTION_H
