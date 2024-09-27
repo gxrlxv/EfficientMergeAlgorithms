c023f0f897f89eef157fe636530d7497bfd62673
Skijetler <xerminatrix@gmail.com>
2024-09-27 21:07:45 +0300

    fix hla + add two way merge

--- src/hla.cpp
+++ src/hla.cpp
@@ -5,89 +5,65 @@
 #include <iostream>
 #include <vector>
 
-template <typename IterContainer, typename T>
-typename IterContainer::iterator lower_bound(IterContainer& arr, T val) {
-    auto [left, right] = std::array<typename IterContainer::iterator, 2>{{arr.begin(), arr.end()}};
-
-    while (left < right) {
-        typename IterContainer::iterator mid = left + std::distance(left, right) / 2;
-        
-        if (*mid < val) {
-            left = std::next(mid);
-        } else {
-            right = mid;
-        }
-    }
-
-    if (left != arr.begin()) {
-        return --left;
-    }
-
-    return arr.begin();
-}
-
 template <typename IterContainer>
 IterContainer hla(const IterContainer& a, const IterContainer& b) {
-    auto [a_begin, a_end, b_begin, b_end] = std::array<typename IterContainer::const_iterator, 4>{{a.begin(), a.end(), b.begin(), b.end()}};
-    IterContainer r(a.size() + b.size());
+    auto [a_left, a_right, b_left, b_right] = std::array<typename IterContainer::const_iterator, 4>{{a.begin(), a.end(), b.begin(), b.end()}};
+    IterContainer r(a.size() + b.size()); // Resulting vector
+
+    typename IterContainer::iterator k_iter = r.end(); // Pointer for insertion
     
-    while (a_begin != a_end && b_begin != b_end) {
+    while (a_left != a_right && b_left != b_right) {
         // H1
         int t;
-        if (a_end > b_end)
-            t = std::floor(std::log2(std::distance(a_begin, a_end) / std::distance(b_begin, b_end)));
+        auto [m, n] = std::array<int, 2>{{static_cast<int>(std::distance(a_left, a_right)), static_cast<int>(std::distance(b_left, b_right))}};
+        if (m > n)
+            t = std::floor(std::log2(m / n));
         else
-            t = std::floor(std::log2(std::distance(b_begin, b_end) / std::distance(a_begin, a_end)));
+            t = std::floor(std::log2(n / m));
         
         // H2
-        if (*(a_end - 1) < *(b_end - std::pow(2, t))) {
-            std::copy(b_end - std::pow(2, t), b_end, r.end() - std::distance(b_end - std::pow(2, t), b.end())); // copy b elements from (b_end - std::pow(2, t) + 1) to b_end
-            b_end -= std::pow(2, t);
+        if (*(a_right - 1) < *(b_right - std::pow(2, t))) {
+            k_iter -= std::pow(2, t);
+            std::copy(b_right - std::pow(2, t), b_right, k_iter); // copy elements from (b_right - std::pow(2, t) + 1) to b_right
+            b_right -= std::pow(2, t);
             continue;
         }
 
         // H3
-        typename IterContainer::iterator k_iter = lower_bound(r, *(a_end - 1));
-        if (*k_iter < *(a_end - 1)) {
-            *k_iter = *(a_end - 1);
-            a_end--;
-            b_end = b_begin + std::distance(r.begin(), k_iter);
+        if (*(b_right - 1) < *(a_right - 1)) {
+            *(--k_iter) = *(a_right - 1);
+            a_right--;
             continue;
-        } else {
-            *k_iter = *(a_end - 1);
         }
 
         // H4
-        if (*(b_end - 1) < *(a_end - std::pow(2, t))) {
-            std::copy(a_end - std::pow(2, t), a_end, r.end() - std::distance(a_end - std::pow(2, t), a.end())); // copy a elements from (a_end - std::pow(2, t) + 1) to a_end
-            a_end -= std::pow(2, t);
+        if (*(b_right - 1) < *(a_right - std::pow(2, t))) {
+            k_iter -= std::pow(2, t);
+            std::copy(a_right - std::pow(2, t), a_right, k_iter); // copy elements from (a_right - std::pow(2, t) + 1) to a_right
+            a_right -= std::pow(2, t);
             continue;
         }
 
         // H5
-        k_iter = lower_bound(r, *(b_end - 1));
-        if (*k_iter < *(b_end - 1)) {
-            *k_iter = *(b_end - 1);
-            b_end--;
-            a_end = a_begin + std::distance(r.begin(), k_iter);
+        if (*(a_right - 1) < *(b_right - 1)) {
+            *(--k_iter) = *(b_right - 1);
+            b_right--;
             continue;
-        } else {
-            *k_iter = *(b_end - 1);
         }
     }
     
-    if (a_begin == a_end) {
-        std::copy(b_begin, b_end, r.begin());
+    if (a_left == a_right) {
+        std::copy(b_left, b_right, r.begin());
     } else {
-        std::copy(a_begin, a_end, r.begin());
+        std::copy(a_left, a_right, r.begin());
     }
     
     return r;
 }
 
 int main() {
-    std::vector<int> x = {1, 6};
-    std::vector<int> y = {2, 5, 7, 10};
+    std::vector<int> x = {1, 2, 3};
+    std::vector<int> y = {4, 5, 6, 7};
 
     std::cout << "Vector x: \n";
     for (auto & e : x)
