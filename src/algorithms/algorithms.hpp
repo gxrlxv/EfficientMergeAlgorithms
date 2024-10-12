41121d215cfd1032fff7f4d17ec4c43ca6a36a16
Sergei Gorlov <71068953+gxrlxv@users.noreply.github.com>
2024-10-12 19:54:13 +0300

    init framework (#5)
    * init framework
    
    * fix func name
    
    * move implementation to hpp file

--- src/algorithms/algorithms.hpp
+++ src/algorithms/algorithms.hpp
@@ -1,10 +1,61 @@
-// Hwang-Lin Merging Algorithm (Art of Computer Programming Volume 3 Sorting & Searching)
-#include <array>
-#include <algorithm>
-#include <cmath>
-#include <iostream>
-#include <vector>
+#ifndef ALGORITHMS_H
+#define ALGORITHMS_H
 
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <array>
+#include <cmath>
+
+// Standard merging of 2 sorted arrays with n and m lenghts
+template <typename IterContainer>
+IterContainer two_way_merge(const IterContainer& a, const IterContainer& b) {
+    auto [a_left, a_right, b_left, b_right] = std::array<typename IterContainer::const_iterator, 4>{{a.begin(), a.end(), b.begin(), b.end()}};
+    IterContainer r(a.size() + b.size()); // Resulting vector
+
+    typename IterContainer::iterator k_iter = r.begin(); // Pointer for insertion
+
+    while (a_left != a_right && b_left != b_right) {
+        if (*a_left <= *b_left) {
+            *k_iter = *a_left;
+            a_left++;
+        } else {
+            *k_iter = *b_left;
+            b_left++;
+        }
+        k_iter++;
+    }
+
+    if (a_left == a_right) {
+        std::copy(b_left, b_right, k_iter);
+    } else {
+        std::copy(a_left, a_right, k_iter);
+    }
+
+    return r;
+}
+
+template <typename IterContainer, typename T>
+void binary_insertion(IterContainer& arr, const T& elem) {
+    typename IterContainer::iterator l = arr.begin();
+    typename IterContainer::iterator u = arr.end();
+
+    while (std::distance(l, u) > 0) {
+        typename IterContainer::iterator j = l;
+        int mid = std::distance(l, u) / 2;
+        std::advance(j, mid);
+
+        if (*j < elem) {
+            l = std::next(j);
+        } else {
+            u = j;
+        }
+    }
+
+    arr.insert(l, elem);
+}
+
+// Hwang-Lin Merging Algorithm (Art of Computer Programming Volume 3 Sorting & Searching)
 template <typename IterContainer>
 IterContainer hla(const IterContainer& a, const IterContainer& b) {
     auto [a_left, a_right, b_left, b_right] = std::array<typename IterContainer::const_iterator, 4>{{a.begin(), a.end(), b.begin(), b.end()}};
@@ -61,26 +112,4 @@ IterContainer hla(const IterContainer& a, const IterContainer& b) {
     return r;
 }
 
-int main() {
-    std::vector<int> x = {1, 2, 3};
-    std::vector<int> y = {4, 5, 6, 7};
-
-    std::cout << "Vector x: \n";
-    for (auto & e : x)
-        std::cout << e << " ";
-    std::cout << std::endl;
-
-    std::cout << "Vector y: \n";
-    for (auto & e : y)
-        std::cout << e << " ";
-    std::cout << std::endl;
-
-    std::vector<int> z = hla(x, y);
-
-    std::cout << "Resulting vector z: \n";
-    for (auto & e : z)
-        std::cout << e << " ";
-    std::cout << std::endl;
-
-    return 0;
-}
+#endif // ALGORITHMS_H
---
+++ src/framework/framework.h
@@ -0,0 +1,8 @@
+#ifndef FRAMEWORK_H
+#define FRAMEWORK_H
+
+#include <vector>
+
+void generate_sorted_vectors(int size_a, int size_b, std::vector<int>& a, std::vector<int>& b, std::vector<int>& result);
+
+#endif // FRAMEWORK_H
