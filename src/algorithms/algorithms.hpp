46292ffa55d2a30e17611d41bed5c05eb724f5fc
Sergei Gorlov <gorlov1207@gmail.com>
2024-11-28 20:20:04 +0300

    fixed update of n

--- src/algorithms/algorithms.hpp
+++ src/algorithms/algorithms.hpp
@@ -233,8 +233,8 @@ IterContainer hl_static(IterContainer& a, IterContainer& b) {
             // Insert A[m-1] into B at position q + 1
             b.insert(b.begin() + q + 1, a[m - 1]);
 
-            n = n - q; 
-            m = m - 1;
+            n -=  n - q ;
+            m -=  1;
         }
     } 
 
